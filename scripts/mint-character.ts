0e85039d63c99ef639ec7a41ffb0e28442102733
PING <pingdaze@protonmail.com>
2022-08-11 15:51:52 -0400

    :sparkles: add test minting for all legacy pills

--- scripts/mint-character.ts
+++ scripts/mint-character.ts
@@ -3,6 +3,7 @@ import { network, ethers } from "hardhat";
 import { BigNumber } from "@ethersproject/bignumber";
 import {CharacterValidator, WearablesValidator, SelectableOptions, Core1155, Core721, AugmentsValidator, Characters} from "../typechain-types";
 import charDeploymant from "./deploy-args/char-mock-deployment.json"
+import testMintData from "../data/test-mints.json";
 
 const mockShadowPaktPill = ethers.BigNumber.from(
   "0xD00000000000000940000000000000001"
@@ -50,87 +51,26 @@ async function main() {
       console.log("Approved legacy contract for owner");
 
     }
+    for(let i = 0; i < testMintData.length; i++) {
+      let legacyPills: BigNumber[] = testMintData[i].legacyPills.map(x => ethers.BigNumber.from(x));
+      let collabPills: BigNumber[] = testMintData[i].collabPills.map(x => ethers.BigNumber.from(x));
 
-    let legacyPills: BigNumber[] = [
-      BigZero,
-      BigZero,
-      BigZero,
-      BigZero,
-      BigZero,
-      // BigNumber.from("57896044618658097711785492504343953927315557066662158947172050052883968688129"),
-      // BigNumber.from("57896044618658097711785492504343953929016968901266851264415136113747971538945"),
-      // BigNumber.from("57896044618658097711785492504343953927315557066662158946876902147704615862273"),
-      // BigNumber.from("57896044618658097711785492504343953927315557066662158946710881451041229897729"),
-      // BigNumber.from("57896044618658097711785492504343953926975274699741220483893142886189295992833"),
-    ];
-    let collabPills: BigNumber[] = [
-      BigZero,
-      BigZero,
-      BigZero,
-      BigZero,
-      BigZero,
-    ];
-    let traitsplus: string[] = [
-      "Pepel",
-      "Aateos",
-      "Interstellar Nomad",
-      "Diamond Hands",
-      "None",
-      "bafybeih53q7lmjzrbg6uycrfne3cfnxqqkfwdrv5julewb2e7qvdkcufz4",
-      "bafybeicdchpng53briinhnrqq54ozr2vmyd7cvwu2wndtcz4sfiugdvyxm",
-      "lime",
-      "bafybeigog5ba76nro55k5wqheqduolkswxg35f3zm7zzi3kt7qqa62c6ny",
-    ] as string[];
-    // receipt = await characterValidator.createCharacter(
-    //   legacyPills,
-    //   collabPills,
-    //   traitsplus,
-    //   {gasPrice: 10000000000}
-    // );
-    // console.log("Character minted: ", receipt.hash);
-    await legacy.mintBatch(owner, [mockShadowPaktPill, mockKirbonitePill], [1, 1], ethers.constants.HashZero);
-    await legacy.setApprovalForAll(characterValidator.address, true);
-    legacyPills = [
-      mockShadowPaktPill,
-      BigZero,
-      BigZero,
-      BigZero,
-      BigZero,
-      // BigNumber.from("57896044618658097711785492504343953929016968901266851264415136113747971538945"),
-      // BigNumber.from("57896044618658097711785492504343953927315557066662158946876902147704615862273"),
-      // BigNumber.from("57896044618658097711785492504343953927315557066662158946710881451041229897729"),
-      // BigNumber.from("57896044618658097711785492504343953926975274699741220483893142886189295992833"),
-    ];
-    collabPills = [
-      BigZero,
-      BigZero,
-      BigZero,
-      BigZero,
-      BigZero,
-    ];
-    // traitsplus = [
-    //   "Hashmonk",
-    //   "Aateos",
-    //   "Patcher",
-    //   "Diamond Hands",
-    //   "None",
-    //   "bafybeiffkfo5hqskknmwb7r3zuz6iqdf3zcscvfmli4z64opkufki4udh4", // head
-    //   "bafybeigv7y7cvlgr3z4f7oabtyzi5vk74fcqjzfulwsdoi6h4beqlbhgp4", // torso
-    //   "bafybeic6vuf2l6ouwg5xnzvi4zqff4ov6woqlhj54urrhatfjunmobsuxm", // larm
-    //   "bafybeiefwn32qzxqyewiadmcx4au2tuexh7pp26wfdl4uf336po7cczn2i", // rarm
-    //   "bafybeig5tb5rxqof5zxp2bolr5dqxhjwc7fqgprng2wc2zu5zhzncxhw5u", // lleg
-    //   "bafybeich2xydybqqxx53sucuswdohm4764boxcqqxywkf27frtnc5f2vr4", // rleg
-    //   "arctic", // color
-    //   "bafybeiauxnajd2vno6dtkbr64qnksjolfoolj7sgiiz6w5aguc3gfrdsqe", // crown
-    //   "bafybeiab7wr5ynymlxqx4ja2kc4t5lnqne37aifcsihyz4qclazimdm3kq", // mask
-    // ] as string[];
-    receipt = await characterValidator.createCharacter(
-      legacyPills,
-      collabPills,
-      traitsplus,
-      {gasPrice: 10000000000}
-    );
-    console.log("Character minted: ", receipt.hash);
+      let traitsplus = testMintData[i].traitsplus;
+
+      const mintArray = legacyPills.concat(collabPills).filter(x => x.gt(BigZero));
+      const quantityArray = new Array(mintArray.length).fill(1);
+      await legacy.mintBatch(owner, mintArray, quantityArray, ethers.constants.HashZero);
+      await legacy.setApprovalForAll(characterValidator.address, true);
+      console.log(mintArray, quantityArray);
+      console.log(legacyPills, collabPills);
+      receipt = await characterValidator.createCharacter(
+        legacyPills,
+        collabPills,
+        traitsplus,
+        {gasPrice: 10000000000}
+      );
+      console.log("Character minted: ", receipt.hash);
+    }
   }
 }
 
