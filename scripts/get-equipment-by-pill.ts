cbaa556e801896f24d36de30d1db11acbdea78f3
PING <pingdaze@protonmail.com>
2022-08-11 16:19:27 -0400

    :fire: quick cleanup

--- scripts/get-equipment-by-pill.ts
+++ scripts/get-equipment-by-pill.ts
@@ -5,7 +5,6 @@ import charDeploymant from "./deploy-args/char-mock-deployment.json"
 import { BigNumber } from "ethers";
 
 
-const id1 = 16;
 const pillIDs =  Array.from({length: 14}, (v, k) => k);
 
 // Replace magic numbers
@@ -13,18 +12,7 @@ async function main() {
   let owner: SignerWithAddress, user1: SignerWithAddress;
   [owner, user1] = await ethers.getSigners();
 
-  let core721 : Core721;
-  let core1155 : Core1155;
-  let characterValidator: CharacterValidator;
   let wearablesValidator: WearablesValidator;
-  let augmentsValidator: AugmentsValidator;
-  let requester: RandomnessRelayL2;
-  let character: CharacterGenMock;
-  let receipt;
-  // This is horribly inneficient, probably don't redeploy these each time?
-  // We get the contract to deploy
-
-  // Grab the signers so we can drop them test tokens
   console.log("Current Network: " + network.name);
 
   console.log("Testing deploy to " + network.name);
