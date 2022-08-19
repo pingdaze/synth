f67c0d100b64c83273ddbd45562ba75bb4831585
PING <pingdaze@protonmail.com>
2022-08-19 15:53:01 -0400

    :sparkles: setup new airdrop round

--- scripts/legacy-airdrop.ts
+++ scripts/legacy-airdrop.ts
@@ -8,13 +8,16 @@ import { BigNumber } from "ethers";
 import charDeploymant from "./deploy-args/char-mock-deployment.json"
 
 
-const testAccounts = [
+const oldTestAccounts = [
   "0x3aD353E2845B96fB1B58C1Ca6CdA46B870dE8D6f",
   "0x1425C88ce0780E5bcE5f405333C81f9336dC52eA",
   "0xe003644d10d4ac0e7651c288305bc593c3f62804",
   "0x0f1c01d98ae190ad2739a6075b21c2343d8b412e",
   "0xc6665eb39d2106fb1DBE54bf19190F82FD535c19",
   "0x053E2aD328147a438d2B136637a213E068200fC7"
+]
+const testAccounts = [
+  "0x446e8c0af98a03ccc7ce4af821362b281e787b47"
 ]
 
 const collabIds = [ 0x1, 0x2, 0x3, 0x4, 0x5, 0x6 ];
@@ -33,18 +36,22 @@ async function main() {
 
 
   let pillIDs: BigNumber[] = [];
-  for(let i = 0; i < testAccounts.length; i++) {
-    const pills = await getLegacyPillsBalances(testAccounts[i], ethers.getDefaultProvider('mainnet'));
-    console.log(pills);
+  for(let i = 0; i < oldTestAccounts.length; i++) {
+    const pills = await getLegacyPillsBalances(oldTestAccounts[i], ethers.getDefaultProvider('mainnet'));
     pills.forEach(pill => {
       pillIDs = pillIDs.concat(pill.ids.map(id => BigNumber.from(id)));
     });
-    const amounts = Array(pillIDs.length).fill(1);
-    await core.mintBatch("0x0f1C01d98AE190Ad2739a6075B21C2343d8b412e", pillIDs, amounts, ethers.constants.HashZero);
-    console.log("Minted", pillIDs.length, "pills for", testAccounts[i]);
+
   }
-  console.log("Core1155 deployed to " + core.address);
-  await core.mintBatch("0x0f1C01d98AE190Ad2739a6075B21C2343d8b412e", collabIds, collabAmounts, ethers.constants.HashZero);
+  const amounts = Array(pillIDs.length).fill(1);
+
+  for(let i = 0; i < testAccounts.length; i++) {
+
+    await core.mintBatch(testAccounts[i], pillIDs, amounts, ethers.constants.HashZero);
+    console.log("Minted", pillIDs.length, "pills for", testAccounts[i]);
+    console.log("Core1155 deployed to " + core.address);
+    await core.mintBatch(testAccounts[i], collabIds, collabAmounts, ethers.constants.HashZero);
+  }
 
 }
 
