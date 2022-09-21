a1a59110805871e820bde5ba85019fbc78332306
PING <pingdaze@protonmail.com>
2022-09-20 23:17:28 -0400

    :hammer: revise get equipment script with new IDS

--- scripts/get-equipment-by-pill.ts
+++ scripts/get-equipment-by-pill.ts
@@ -6,6 +6,23 @@ import { BigNumber } from "ethers";
 
 
 const pillIDs =  Array.from({length: 14}, (v, k) => k);
+const legacyPillToId: { [key:string]: string} = {
+  "genesis": "0x80000000000000000000000000000001000000000000000d0000000000000001",
+  "payback pill": "0x80000000000000000000000000000002000000000000000c0000000000000001",
+  "prodpill": "0x8000000000000000000000000000000300000000000000000000000000000003",
+  "unipill": "0x80000000000000000000000000000004000000000000000c0000000000000001",
+  "hypepill":  "0x8000000000000000000000000000000500000000000000040000000000000001",
+  "rektpill":  "0x80000000000000000000000000000006000000000000002a0000000000000001",
+  "memfruit":  "0x80000000000000000000000000000007000000000000000d0000000000000001",
+  "synth_memwraith":  "0x80000000000000000000000000000008000000000000000d0000000000000001",
+  "memricorn": "0x80000000000000000000000000000009000000000000000d0000000000000001",
+  "memsnake":  "0x8000000000000000000000000000000a000000000000000d0000000000000001",
+  "ratspill": "0x8000000000000000000000000000000b00000000000000110000000000000001",
+  "egodeth_ratspill": "0x8000000000000000000000000000000b00000000000000030000000000000001",
+  "kirbonite": "0x8000000000000000000000000000000c000000000000000d0000000000000001",
+  "shadowpak": "0x8000000000000000000000000000000d000000000000000d0000000000000001",
+  "mirrorpill":  "0x8000000000000000000000000000000e000000000000000d0000000000000001"
+}
 const pillNames = [
   "genesis",
   "payback pill",
@@ -36,9 +53,9 @@ async function main() {
   [owner, user1] = await ethers.getSigners();
   console.log("Signing as " + owner.address);
   wearablesValidator = await ethers.getContractAt('WearablesValidator', charDeploymant.ArbRinkeby.WearablesValidator) as WearablesValidator;
-  for(let i = 1; i < pillIDs.length; i++) {
-    const cid = await wearablesValidator.getEquipmentFromPill(BigNumber.from(pillIDs[i]), 1);
-    console.log(`Pills ID ${pillIDs[i]} ${pillNames[i -1]}  CIDs: ${cid}`);
+  for(const pill in legacyPillToId) {
+    const cid = await wearablesValidator.getEquipmentFromPill(BigNumber.from(legacyPillToId[pill]), 1);
+    console.log(`Pills ID ${legacyPillToId[pill]} ${pill}  CIDs: ${cid}`);
   }
     
 }
