c213105010723dc7a7857c7669a2dfe0560356f3
PING <pingdaze@protonmail.com>
2022-10-20 13:28:54 -0400

    :hammer: character mint scripts

--- scripts/mint-character.ts
+++ scripts/mint-character.ts
@@ -1,6 +1,6 @@
 import { network, ethers } from "hardhat";
 import { BigNumber } from "@ethersproject/bignumber";
-import {CharacterValidator, WearablesValidator, SelectableOptions, Core1155, Core721, AugmentsValidator, Characters} from "../typechain-types";
+import {CharacterValidatorV2, WearablesValidator, SelectableOptionsV2, Core1155, Core721, AugmentsValidator, CharactersV2} from "../typechain-types";
 import charDeploymant from "./deploy-args/char-mock-deployment.json"
 
 const BigZero = ethers.BigNumber.from("0");
@@ -10,12 +10,12 @@ async function main() {
   let core721 : Core721;
   let core1155 : Core1155;
   let legacy : Core1155;
-  let characterValidator: CharacterValidator;
+  let characterValidator: CharacterValidatorV2;
   let wearablesValidator: WearablesValidator;
   let augmentsValidator: AugmentsValidator;
-  let character: Characters;
+  let character: CharactersV2;
   let receipt, owner;
-  let options: SelectableOptions;
+  let options: SelectableOptionsV2;
   owner = (await ethers.getSigners())[0].address;
   const mockShadowPaktPill = ethers.BigNumber.from(
     "0xD00000000000000940000000000000001"
@@ -27,36 +27,17 @@ async function main() {
   const balance = await ethers.provider.getBalance(owner);
   console.log("Owner balance: ", ethers.utils.formatEther(balance));
   console.log("Network: " + network.name);
-  core721 = await ethers.getContractAt('Core721', charDeploymant.ArbGorli.Core721) as Core721;
-  core1155 = await ethers.getContractAt('Core1155', charDeploymant.ArbGorli.Core1155) as Core1155;
-  characterValidator = await ethers.getContractAt('CharacterValidator', charDeploymant.ArbGorli.CharacterValidator) as CharacterValidator;
-  wearablesValidator = await ethers.getContractAt('WearablesValidator', charDeploymant.ArbGorli.WearablesValidator) as WearablesValidator;
-  augmentsValidator = await ethers.getContractAt('AugmentsValidator', charDeploymant.ArbGorli.AugmentsValidator) as AugmentsValidator;
-  character = await ethers.getContractAt('Characters', charDeploymant.ArbGorli.Characters) as Characters;
+  core721 = await ethers.getContractAt('Core721', charDeploymant.ArbGorliV2.Core721) as Core721;
+  core1155 = await ethers.getContractAt('Core1155', charDeploymant.ArbGorliV2.Core1155) as Core1155;
+  characterValidator = await ethers.getContractAt('CharacterValidatorV2', charDeploymant.ArbGorliV2.CharacterValidatorV2) as CharacterValidatorV2;
+  wearablesValidator = await ethers.getContractAt('WearablesValidator', charDeploymant.ArbGorliV2.WearablesValidator) as WearablesValidator;
+  augmentsValidator = await ethers.getContractAt('AugmentsValidator', charDeploymant.ArbGorliV2.AugmentsValidator) as AugmentsValidator;
+  character = await ethers.getContractAt('CharactersV2', charDeploymant.ArbGorliV2.CharactersV2) as CharactersV2;
   legacy = await ethers.getContractAt("Core1155", charDeploymant.ArbGorli.Pills1155) as Core1155;
 
-  options = await ethers.getContractAt('SelectableOptions', charDeploymant.ArbGorli.SelectableOptions) as SelectableOptions;
+  options = await ethers.getContractAt('SelectableOptionsV2', charDeploymant.ArbGorliV2.SelectableOptionsV2) as SelectableOptionsV2;
   if(network.name === "arbgorli" ) {   
-    if(await legacy.isApprovedForAll(owner, character.address) == false) {
-      console.log("Approving legacy contract for owner");
-      await legacy.setApprovalForAll(character.address, true);
-      console.log("Approved legacy contract for owner");
 
-    }
-    let legacyPills: BigNumber[] = [
-      BigNumber.from("0x8000000000000000000000000000000300000000000000000000000000000003"),
-      BigZero,
-      BigZero,
-      BigZero,
-      BigZero,
-    ]
-    let collabPills: BigNumber[] = [
-      BigZero,
-      BigZero,
-      BigZero,
-      BigZero,
-      BigZero,
-    ]
     let traitsplus = [
       "Pepel",
       "Durn",
@@ -69,17 +50,8 @@ async function main() {
       "bafybeigroraemt2yujiqgztpaya5q6lzp3tswy7sgkaicq5phdswbkptqi"
     ]
 
-    const mintArray = legacyPills.concat(collabPills).filter(x => x.gt(BigZero));
-    const quantityArray = new Array(mintArray.length).fill(1);
-    await legacy.mintBatch(owner, mintArray, quantityArray, ethers.constants.HashZero);
-    console.log("Minted legacy pills");
-    await legacy.setApprovalForAll(characterValidator.address, true);
-    console.log("Approved legacy pills for character validator");
-    console.log(mintArray, quantityArray);
-    console.log(legacyPills, collabPills);
+
     receipt = await characterValidator.createCharacter(
-      legacyPills,
-      collabPills,
       traitsplus,
       {gasPrice: 10000000000}
     );
--- scripts/mint-characters.ts
+++ scripts/mint-characters.ts
@@ -17,7 +17,6 @@ async function main() {
 
   let core721 : Core721;
   let core1155 : Core1155;
-  let legacy : Core1155;
   let characterValidator: CharacterValidator;
   let wearablesValidator: WearablesValidator;
   let augmentsValidator: AugmentsValidator;
@@ -25,44 +24,24 @@ async function main() {
   let receipt, owner;
   let options: SelectableOptions;
   owner = (await ethers.getSigners())[0].address;
-  const mockShadowPaktPill = ethers.BigNumber.from(
-    "0xD00000000000000940000000000000001"
-  );
-  const mockKirbonitePill = ethers.BigNumber.from(
-    "0xC00000000000000650000000000000001"
-  );
+
   console.log("owner", owner);
   const balance = await ethers.provider.getBalance(owner);
   console.log("Owner balance: ", ethers.utils.formatEther(balance));
   console.log("Network: " + network.name);
-  core721 = await ethers.getContractAt('Core721', charDeploymant.ArbGorli.Core721) as Core721;
-  core1155 = await ethers.getContractAt('Core1155', charDeploymant.ArbGorli.Core1155) as Core1155;
-  characterValidator = await ethers.getContractAt('CharacterValidator', charDeploymant.ArbGorli.CharacterValidator) as CharacterValidator;
-  wearablesValidator = await ethers.getContractAt('WearablesValidator', charDeploymant.ArbGorli.WearablesValidator) as WearablesValidator;
-  augmentsValidator = await ethers.getContractAt('AugmentsValidator', charDeploymant.ArbGorli.AugmentsValidator) as AugmentsValidator;
-  character = await ethers.getContractAt('Characters', charDeploymant.ArbGorli.Characters) as Characters;
-  legacy = await ethers.getContractAt("Core1155", charDeploymant.ArbGorli.Pills1155) as Core1155;
+  core721 = await ethers.getContractAt('Core721', charDeploymant.ArbGorliV2.Core721) as Core721;
+  core1155 = await ethers.getContractAt('Core1155', charDeploymant.ArbGorliV2.Core1155) as Core1155;
+  characterValidator = await ethers.getContractAt('CharacterValidator', charDeploymant.ArbGorliV2.CharacterValidatorV2) as CharacterValidator;
+  wearablesValidator = await ethers.getContractAt('WearablesValidator', charDeploymant.ArbGorliV2.WearablesValidator) as WearablesValidator;
+  augmentsValidator = await ethers.getContractAt('AugmentsValidator', charDeploymant.ArbGorliV2.AugmentsValidator) as AugmentsValidator;
+  character = await ethers.getContractAt('Characters', charDeploymant.ArbGorliV2.CharactersV2) as Characters;
 
-  options = await ethers.getContractAt('SelectableOptions', charDeploymant.ArbGorli.SelectableOptions) as SelectableOptions;
+  options = await ethers.getContractAt('SelectableOptions', charDeploymant.ArbGorliV2.SelectableOptionsV2) as SelectableOptions;
   if(network.name === "arbgorli" ) {   
-    if(await legacy.isApprovedForAll(owner, character.address) == false) {
-      console.log("Approving legacy contract for owner");
-      await legacy.setApprovalForAll(character.address, true);
-      console.log("Approved legacy contract for owner");
-
-    }
     for(let i = 0; i < testMintData.length; i++) {
       let legacyPills: BigNumber[] = testMintData[i].legacyPills.map(x => ethers.BigNumber.from(x));
       let collabPills: BigNumber[] = testMintData[i].collabPills.map(x => ethers.BigNumber.from(x));
-
       let traitsplus = testMintData[i].traitsplus;
-
-      const mintArray = legacyPills.concat(collabPills).filter(x => x.gt(BigZero));
-      const quantityArray = new Array(mintArray.length).fill(1);
-      await legacy.mintBatch(owner, mintArray, quantityArray, ethers.constants.HashZero);
-      await legacy.setApprovalForAll(characterValidator.address, true);
-      console.log(mintArray, quantityArray);
-      console.log(legacyPills, collabPills);
       receipt = await characterValidator.createCharacter(
         legacyPills,
         collabPills,
