472ebd976e50d15d4416a72c2b2f35341312d718
PING <pingdaze@protonmail.com>
2022-11-15 22:30:32 -0500

    :poop: unguard deploy function

--- scripts/charactersV2.ts
+++ scripts/charactersV2.ts
@@ -59,69 +59,67 @@ async function main() {
   let portalPillAddr = "0xa16891897378a82e9f0ad44a705b292c9753538c";
   let portalPill = await ethers.getContractAt('Core1155', portalPillAddr) as Core1155;
 
-  if(network.name === "arbrinkeby" || network.name === "arbgorli") {
-    core721 = (await deployCore721()) as Core721;
-    await core721.deployed();
-    console.log("Core721 address: ", core721.address);
-    core1155 = (await deployCore1155()) as Core1155;
-    await core1155.deployed();
-    console.log("Core1155 address: ", core1155.address);
-    booster1155 = (await deployCore1155()) as Core1155;
-    await booster1155.deployed();
-    console.log("Booster 1155 address: ", core1155.address);
-    options = (await deploySelectableOptionsV2(
-    )) as SelectableOptionsV2;
-    await options.deployed();
-    console.log("SelectableOptions address: ", options.address);
-    // In production instances the IDs must line up correctly
-    character = (await deployCharacterV2(core721, options)) as CharactersV2;
-    await character.deployed();
-    console.log("Characters address: ", character.address);
-    wearablesValidator = (await deployWearablesValidator(
-      core1155,
-      character
-    )) as WearablesValidator;
-    await wearablesValidator.deployed();
-    receipt = await character.setWearables(wearablesValidator.address);
-    await receipt.wait();
-    console.log("WearablesValidator address: ", wearablesValidator.address);
-    augmentsValidator = (await deployAugmentsValidator(
-      core1155,
-      character
-    )) as AugmentsValidator;
-    await augmentsValidator.deployed();
-    console.log("AugmentsValidator address: ", augmentsValidator.address);
-    requester = (await deployRequester()) as RandomnessRelayL2;
-    await requester.deployed();
-    console.log("Requester address: ", requester.address);
+  core721 = (await deployCore721()) as Core721;
+  await core721.deployed();
+  console.log("Core721 address: ", core721.address);
+  core1155 = (await deployCore1155()) as Core1155;
+  await core1155.deployed();
+  console.log("Core1155 address: ", core1155.address);
+  booster1155 = (await deployCore1155()) as Core1155;
+  await booster1155.deployed();
+  console.log("Booster 1155 address: ", core1155.address);
+  options = (await deploySelectableOptionsV2(
+  )) as SelectableOptionsV2;
+  await options.deployed();
+  console.log("SelectableOptions address: ", options.address);
+  // In production instances the IDs must line up correctly
+  character = (await deployCharacterV2(core721, options)) as CharactersV2;
+  await character.deployed();
+  console.log("Characters address: ", character.address);
+  wearablesValidator = (await deployWearablesValidator(
+    core1155,
+    character
+  )) as WearablesValidator;
+  await wearablesValidator.deployed();
+  receipt = await character.setWearables(wearablesValidator.address);
+  await receipt.wait();
+  console.log("WearablesValidator address: ", wearablesValidator.address);
+  augmentsValidator = (await deployAugmentsValidator(
+    core1155,
+    character
+  )) as AugmentsValidator;
+  await augmentsValidator.deployed();
+  console.log("AugmentsValidator address: ", augmentsValidator.address);
+  requester = (await deployRequester()) as RandomnessRelayL2;
+  await requester.deployed();
+  console.log("Requester address: ", requester.address);
 
-    characterValidator = (await deployCharacterValidatorV2(
-      core721,
-      options,
-      wearablesValidator,
-      character,
-      portalPill
-    )) as CharacterValidatorV2;
-    await characterValidator.deployed();
-    console.log("CharacterValidator address: ", characterValidator.address);
-    receipt = await character.setValidator(characterValidator.address);
-    await receipt.wait();
-    console.log("CharacterValidator set");
-    receipt = await core721.addValidator(characterValidator.address, coreIds);
-    await receipt.wait();
-    console.log("WearablesValidator set");
-    receipt = await core1155.addValidator(wearablesValidator.address, coreIds);
-    await receipt.wait();
-    console.log("Character Validator Installed");
-    receipt = await booster1155.addValidator(characterValidator.address, coreIds);
-    await receipt.wait();
-    console.log("Booster Validator Installed");
-    await pushOptions(
-      options.address,
-      wearablesValidator.address,
-      augmentsValidator.address
-    );
-  }
+  characterValidator = (await deployCharacterValidatorV2(
+    core721,
+    options,
+    wearablesValidator,
+    character,
+    portalPill
+  )) as CharacterValidatorV2;
+  await characterValidator.deployed();
+  console.log("CharacterValidator address: ", characterValidator.address);
+  receipt = await character.setValidator(characterValidator.address);
+  await receipt.wait();
+  console.log("CharacterValidator set");
+  receipt = await core721.addValidator(characterValidator.address, coreIds);
+  await receipt.wait();
+  console.log("WearablesValidator set");
+  receipt = await core1155.addValidator(wearablesValidator.address, coreIds);
+  await receipt.wait();
+  console.log("Character Validator Installed");
+  receipt = await booster1155.addValidator(characterValidator.address, coreIds);
+  await receipt.wait();
+  console.log("Booster Validator Installed");
+  await pushOptions(
+    options.address,
+    wearablesValidator.address,
+    augmentsValidator.address
+  );
 }
   
 main()
